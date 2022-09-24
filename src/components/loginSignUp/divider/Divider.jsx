43f03f05262d835e56d4a44dd64ce0b34cbdb673
Fakhr <fakhretarabay@gmail.com>
2022-09-24 15:48:03 +0200

    finished login/signUp

---
+++ src/components/loginSignUp/divider/Divider.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Divider from "@mui/material/Divider";
+import css from './Divider.module.css'
+const DividerX = () => {
+  return (
+    <Divider
+      sx={{
+        width: "100%",
+        "&::before, &::after": {
+          borderColor: "#323745",
+        },
+      }}
+    >
+      <span className={css.dividerOr}>or</span>
+    </Divider>
+  );
+};
+
+export default DividerX;
---
+++ src/components/loginSignUp/title/Title.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import css from "./Title.module.css";
+
+const Title = ({ isUp, setIsuP }) => {
+  return (
+    <div className={css.title}>
+      <h1>Get's started.</h1>
+      {isUp ? (
+        <span className={css.reg}>
+          Already have an account?
+          <span className={css.med}>
+            <a className={css.a} onClick={() => setIsuP(!isUp)}>
+              {" "}
+              Sign in
+            </a>
+          </span>
+        </span>
+      ) : (
+        <span className={css.reg}>
+          Don't have an account?
+          <span className={css.med}>
+            <a className={css.a} onClick={() => setIsuP(!isUp)}>
+              {" "}
+              Sign up
+            </a>
+          </span>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Title;
